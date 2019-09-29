1dbd2b206e447c4559734135a324b1651f3149d4
Jvalana Caryl Shankar <jvalanacarylshankar@dhcp-128-189-72-90.ubcsecure.wireless.ubc.ca>
2019-09-29 14:24:03 -0700

    Adding salmon template

---
+++ build/CMakeFiles/salmon.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/salmon.dir/src/common.cpp.o"
+  "CMakeFiles/salmon.dir/src/fish.cpp.o"
+  "CMakeFiles/salmon.dir/src/main.cpp.o"
+  "CMakeFiles/salmon.dir/src/pebbles.cpp.o"
+  "CMakeFiles/salmon.dir/src/salmon.cpp.o"
+  "CMakeFiles/salmon.dir/src/turtle.cpp.o"
+  "CMakeFiles/salmon.dir/src/water.cpp.o"
+  "CMakeFiles/salmon.dir/src/world.cpp.o"
+  "salmon"
+  "salmon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/salmon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
