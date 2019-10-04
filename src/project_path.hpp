43aac8b2b77d70f6aad946c252133d03c88dbaef
UBC Student <viven304@students.cs.ubc.ca>
2019-10-03 20:43:31 -0700

    added new wanderer class files

--- src/project_path.hpp
+++ src/project_path.hpp
@@ -2,4 +2,4 @@
 
 // Please don't change the content of this header
 
-#define PROJECT_SOURCE_DIR "C:/Users/viven/OneDrive/Desktop/team9-jvalana-moving/"
+#define PROJECT_SOURCE_DIR "C:/Users/viven/OneDrive/Documents/the_chameleon/"
