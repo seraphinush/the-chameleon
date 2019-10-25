dd4e5bd1fbbdb98b15b9c8028e9fc67ab0074116
wnsrl <wnsrl@students.ubc.ca>
2019-10-25 18:29:03 +0900

    Updated header format

--- src/control_screen.hpp
+++ src/control_screen.hpp
@@ -17,11 +17,7 @@ class ControlScreen : public Entity
 
 public:
 	bool init();
-
 	void destroy();
-
 	void update(unsigned int game_state);
-
-	// projection is the 2D orthographic projection matrix
 	void draw(const mat3 &projection) override;
 };
