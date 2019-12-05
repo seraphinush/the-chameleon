8ebab9479ac2869b37a9dca28ac5ccc1e30bde31
wnsrl <wnsrl@students.cs.ubc.ca>
2019-12-05 10:42:00 +0900

    Added color change sfx

--- src/char.cpp
+++ src/char.cpp
@@ -26,15 +26,16 @@ bool Char::init(vec2 spos)
 	}
 
 	m_sfx_bump = Mix_LoadWAV(audio_path("char_bump.wav"));
+	m_sfx_color_change = Mix_LoadWAV(audio_path("char_color_change.wav"));
 	m_sfx_dead = Mix_LoadWAV(audio_path("char_dead.wav"));
 	m_sfx_walk = Mix_LoadWAV(audio_path("char_walk.wav"));
 	
-	if (m_sfx_bump == nullptr || m_sfx_dead == nullptr || m_sfx_walk == nullptr)
+	if (m_sfx_bump == nullptr || 
+	  m_sfx_color_change == nullptr || 
+	  m_sfx_dead == nullptr || 
+		m_sfx_walk == nullptr)
 	{
-		fprintf(stderr, "Failed to load sounds\n %s\n %s\n &s\n make sure the data directory is present\n",
-				audio_path("char_bump.wav"),
-				audio_path("char_dead.wav"),
-				audio_path("char_walk.wav"));
+		fprintf(stderr, "Failed to load char sfx\n");
 		return false;
 	}
 
@@ -457,6 +458,8 @@ int Char::get_direction() const
 // 1: red, 2: green; 3: blue; 4: yellow;
 void Char::set_color(int color)
 {
+	if (m_color != color)
+		Mix_PlayChannel(-1, m_sfx_color_change, 0);
 	m_color = color;
 }
 
